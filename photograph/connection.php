dc250044ab6ae04f0a6200431a6694be1d727067
Dave89rr <daleriro@msn.com>
2016-04-22 14:16:00 -0400

    General Push

--- photograph/connection.php
+++ photograph/connection.php
@@ -3,10 +3,10 @@
 $hostname = "localhost";
 $username = "root";
 $password = "";
-$dbname = "mytestdatabase";
+$dbname = "photodb";
 
 //making the connection to mysql
 $dbc = mysqli_connect($hostname, $username, $password, $dbname) OR die("could not connect to database, ERROR: ".mysqli_connect_error());
 
-echo "you are connected to ".$dbname." Database";
+// echo "you are connected to ".$dbname." Database";
 ?>
--- photograph/processingemployees.php
+++ photograph/processingemployees.php
@@ -2,26 +2,28 @@
 <?php
 include('connection.php');
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-$EID = $_POST['EmployeeID'];
-$EFN = $_POST['EmpFirstName'];
-$ELN = $_POST['EmpLastName'];
-$EA = $_POST['EmpAddress'];
-$EC = $_POST['EmpCity'];
-$ES = $_POST['EmpState'];
-$EZ = $_POST['EmpZIP'];
-$EP = $_POST['EmpPhone'];
-$ECP = $_POST['EmpCellPhone'];
+// $EID = $_POST['EMPLOYEEID'];
+$EFN = $_POST['EMPFIRSTNAME'];
+$ELN = $_POST['EMPLASTNAME'];
+$EA = $_POST['EMPADDRESS'];
+$EC = $_POST['EMPCITY'];
+$ES = $_POST['EMPSTATE'];
+$EZ = $_POST['EMPZIP'];
+$EP = $_POST['EMPPHONE'];
+$ECP = $_POST['EMPCELLPHONE'];
+$EEM = $_POST['EMPEMAIL'];
+$EPAS = $_POST['EMPPASS'];
 
 //if values are not empty, proceed to store them in the database
-if(!empty($EID) && !empty($EFN) && !empty($ELN) && !empty($EA) && !empty($EC) && !empty($ES) && !empty($EZ) && !empty($EP) && !empty($ECP))
+if(!empty($EFN) && !empty($ELN) && !empty($EA) && !empty($EZ) && !empty($ECP) && !empty($EEM) && !empty($EPAS))
 {
-        mysqli_query($dbc, "INSERT INTO employees(EmployeeID, EmpFirstName, EmpLastName, EmpAddress, EmpCity,  EmpState, EmpZIP, EmpPhone, EmpCellPhone )
-        VALUES ('$EID', '$EFN', '$ELN', '$EA', '$EC','$ES','$EZ','$EP','$ECP',)");
-        echo " row  inserted, everything worked fine!";    
-        echo "<a href = 'employeepage.html'>Click here to be redirected to the employee Page. </a>"; 
+        mysqli_query($dbc, "INSERT INTO EMPLOYEES(EMPFIRSTNAME, EMPLASTNAME, EMPADDRESS, EMPCITY,  EMPSTATE, EMPZIP, EMPPHONE, EMPCELLPHONE, EMPEMAIL, EMPPASS )
+        VALUES ('$EFN', '$ELN', '$EA', '$EC','$ES','$EZ','$EP','$ECP', '$EEM', '$EPAS')");
+        echo " row  inserted, everything worked fine!";
+        echo "<a href = 'employeepage.html'>Click here to be redirected to the employee Page. </a>";
 }
-    else{        
-            echo "ERROR: you left some values in blank!";    
+    else{
+            echo "ERROR: you left some values in blank!";
         }
     }
     else
