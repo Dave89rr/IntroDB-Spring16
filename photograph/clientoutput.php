581bf1f5963a84dddd8ccbae8f83b9c569cb475d
Sam <allen.sam19@yahoo.com>
2016-04-22 16:22:20 -0400

    Added client output and updated emp page

---
+++ photograph/clientoutput.php
@@ -0,0 +1,45 @@
+<?php
+
+    include("connection.php");
+    echo "<br>";
+    echo "<h1>Employees</h1>";
+  	echo "<table border='1'>";
+  	echo "<tr>";
+  	echo "<td><b>ID</b></td>";
+  	echo "<td><b>First Name</b></td>";
+    echo "<td><b>Last Name</b></td>";
+    echo "<td><b>Address</b></td>";
+    echo "<td><b>City</b></td>";
+    echo "<td><b>State</b></td>";
+    echo "<td><b>ZIP</b></td>";
+    echo "<td><b>Phone</b></td>";
+    echo "<td><b>Cellphone</b></td>";
+    echo "<td><b>Email</b></td>";
+  	echo "</tr>";
+    $r = mysqli_query($dbc, "SELECT * FROM employees");
+
+
+    while ( $row = mysqli_fetch_array($r))
+    {
+        echo "<tr>";
+    		echo "<td>".$row['EMPLOYEEID']."</td>";
+    		echo "<td>".$row['EMPFIRSTNAME']."</td>";
+        echo "<td>".$row['EMPLASTNAME']."</td>";
+        echo "<td>".$row['EMPADDRESS']."</td>";
+        echo "<td>".$row['EMPCITY']."</td>";
+        echo "<td>".$row['EMPSTATE']."</td>";
+        echo "<td>".$row['EMPZIP']."</td>";
+        echo "<td>".$row['EMPPHONE']."</td>";
+        echo "<td>".$row['EMPCELLPHONE']."</td>";
+        echo "<td>".$row['EMPEMAIL']."</td>";
+    		echo "</tr>";
+        // echo $row['EmployeeID']."/".$row['EmpFirstName']."/".$row['EmpLastName']."/".$row['EmpAdress']."/".$row['EmpCity']."/".$row['EmpState']."/".$row['EmpZip']."/".$row['EmpPhone']."/".$row['EmpCellPhone'];
+        // echo "<br>";
+    }
+mysqli_close($dbc); //always close the connection for security
+echo "</table>";
+echo "<br><br>";
+// echo "database connection closed.";
+
+
+?>
