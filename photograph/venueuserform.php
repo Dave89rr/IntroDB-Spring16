95a3fd3644e27a0a230253889d17918953c6ca49
Dave89rr <daleriro@msn.com>
2016-04-22 15:53:48 -0400

    Updates
    Added Venue Name to SQL Creation
    Formatted VenueUserForm

--- photograph/venueuserform.php
+++ photograph/venueuserform.php
@@ -4,16 +4,38 @@
 <title>Add a new venue</title>
 </head>
 <body>
-    <form action="processingvenues.php" method="post">
-    <p>VenueAddress: <input type="text" name="VA" size="20" maxlength="50" /></p>
-    <p>VenueCity: <input type="text" name="VC" size="20" maxlength="50" /></p>
-    <p>VenueState: <input type="text" name= "VS" size="20" maxlength="50" /></p>
-    <p>VenueZIP: <input type="text" name= "VZ" size="20" maxlength="50" /></p>
-    <p>VenueNumber: <input type="text" name="VN" size="20" maxlength="50" /></p>
-    <p>VenueContactName: <input type="text" name="VCN" size="20" maxlength="50" /></p>
-    <p>VenueType: <input type="text" name="VT" size="20" maxlength="50" /></p>
-
-    <p><input type="submit" name="submit" value="Submit" /></p>       
-    </form>
+  <h1>New Venue</h1>
+  <form action="processingvenues.php" method="post">
+    <table>
+      <tr>
+        <td>*Name:</td><td><input type="text" name="VENUENAME" size="20" maxlength="50" /></td>
+      </tr>
+      <tr>
+        <td>*Address:</td><td><input type="text" name="VA" size="20" maxlength="50" /></td>
+      </tr>
+      <tr>
+        <td>City:</td><td><input type="text" name="VC" size="20" maxlength="50" /></td>
+      </tr>
+      <tr>
+        <td>*State:</td><td><input type="text" name= "VS" size="20" maxlength="50" /></td>
+      </tr>
+      <tr>
+        <td>*ZIP:</td><td><input type="text" name= "VZ" size="20" maxlength="50" /></td>
+      </tr>
+      <tr>
+        <td>*Number:</td><td><input type="text" name="VNUM" size="20" maxlength="50" /></td>
+      </tr>
+      <tr>
+        <td>Contact Name:</td><td><input type="text" name="VCN" size="20" maxlength="50" /></td>
+      </tr>
+      <tr>
+        <td>*Type:</td><td><input type="text" name="VT" size="20" maxlength="50" /></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td><input type="submit" name="submit" value="Submit" /></td>
+      </tr>
+    </table>
+  </form>
 </body>
 </html>
