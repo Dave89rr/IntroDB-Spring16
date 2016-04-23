c11cab030e38d36cf6ab8e462d86e37199b1f220
Dave89rr <daleriro@msn.com>
2016-04-23 19:11:27 -0400

    Updated processing files camera and employees

--- photograph/processing/processingcameras.php
+++ photograph/processing/processingcameras.php
@@ -1,26 +1,26 @@
 <?php
 include('../connection.php');
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-$SCount = $_POST['SHOTCOUNT'];
-$CamType = $_POST['CAMERATYPE'];
-$NumBat = $_POST['BATTERIES'];
-$PriLense = $_POST['PRIMARYLENSE'];
-
-//if values are not empty, proceed to store them in the database
-if(!empty($SCount) && !empty($PriLense))
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-		mysqli_query($dbc, "INSERT INTO CAMERAS('CAMERAID', 'SHOTCOUNT', 'CAMERATYPE', 'BATTERIES', 'PRIMARYLENSE')
+	$SCount = $_POST['SHOTCOUNT'];
+	$CamType = $_POST['CAMERATYPE'];
+	$NumBat = $_POST['BATTERIES'];
+	$PriLense = $_POST['PRIMARYLENSE'];
+
+	//if values are not empty, proceed to store them in the database
+	if(!empty($SCount) && !empty($PriLense))
+	{
+		mysqli_query($dbc, "INSERT INTO CAMERAS(SHOTCOUNT, CAMERATYPE, BATTERIES, PRIMARYLENSE)
 		VALUES ('$SCount', '$CamType', '$NumBat', '$PriLense')");
-		echo " row  inserted, everything worked fine!";
-		echo "<a href = '../employeepage.php'>Click here to be redirected to the employee Page. </a>";
-}
-	else{
-			echo "ERROR: you left some values in blank!";
-		}
+		echo "<meta http-equiv='refresh' content='0;url=../employeepage.php'/>";
 	}
 	else
 	{
+		echo "ERROR: you left some values in blank!";
+	}
+}
+else
+{
 	echo "<strong>Please complete the form...</strong>";
 }
-
 ?>
--- photograph/processing/processingemployees.php
+++ photograph/processing/processingemployees.php
@@ -1,34 +1,32 @@
-
 <?php
 include('../connection.php');
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-// $EID = $_POST['EMPLOYEEID'];
-$EFN = $_POST['EMPFIRSTNAME'];
-$ELN = $_POST['EMPLASTNAME'];
-$EA = $_POST['EMPADDRESS'];
-$EC = $_POST['EMPCITY'];
-$ES = $_POST['EMPSTATE'];
-$EZ = $_POST['EMPZIP'];
-$EP = $_POST['EMPPHONE'];
-$ECP = $_POST['EMPCELLPHONE'];
-$EEM = $_POST['EMPEMAIL'];
-$EPAS = $_POST['EMPPASS'];
-
-//if values are not empty, proceed to store them in the database
-if(!empty($EFN) && !empty($ELN) && !empty($EA) && !empty($EZ) && !empty($ECP) && !empty($EEM) && !empty($EPAS))
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-        mysqli_query($dbc, "INSERT INTO EMPLOYEES(EMPFIRSTNAME, EMPLASTNAME, EMPADDRESS, EMPCITY,  EMPSTATE, EMPZIP, EMPPHONE, EMPCELLPHONE, EMPEMAIL, EMPPASS )
-        VALUES ('$EFN', '$ELN', '$EA', '$EC','$ES','$EZ','$EP','$ECP', '$EEM', '$EPAS')");
-        echo " row  inserted, everything worked fine!";
-        echo "<a href = '../employeepage.php'>Click here to be redirected to the employee Page. </a>";
-}
-    else{
-            echo "ERROR: you left some values in blank!";
-        }
-    }
-    else
- {
-    echo "<strong>Please complete the form...</strong>";
-}
+  $EFN = $_POST['EMPFIRSTNAME'];
+  $ELN = $_POST['EMPLASTNAME'];
+  $EA = $_POST['EMPADDRESS'];
+  $EC = $_POST['EMPCITY'];
+  $ES = $_POST['EMPSTATE'];
+  $EZ = $_POST['EMPZIP'];
+  $EP = $_POST['EMPPHONE'];
+  $ECP = $_POST['EMPCELLPHONE'];
+  $EEM = $_POST['EMPEMAIL'];
+  $EPAS = $_POST['EMPPASS'];
 
+  //if values are not empty, proceed to store them in the database
+  if(!empty($EFN) && !empty($ELN) && !empty($EA) && !empty($EZ) && !empty($ECP) && !empty($EEM) && !empty($EPAS))
+  {
+    mysqli_query($dbc, "INSERT INTO EMPLOYEES(EMPFIRSTNAME, EMPLASTNAME, EMPADDRESS, EMPCITY,  EMPSTATE, EMPZIP, EMPPHONE, EMPCELLPHONE, EMPEMAIL, EMPPASS )
+    VALUES ('$EFN', '$ELN', '$EA', '$EC','$ES','$EZ','$EP','$ECP', '$EEM', '$EPAS')");
+    echo "<meta http-equiv='refresh' content='0;url=../employeepage.php'/>";
+  }
+  else
+  {
+    echo "ERROR: you left some values in blank!";
+  }
+}
+else
+{
+  echo "<strong>Please complete the form...</strong>";
+}
 ?>
