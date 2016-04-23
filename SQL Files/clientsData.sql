5b43787400e80066175b885861bca56e0eec23a5
Dave89rr <daleriro@msn.com>
2016-04-23 18:11:25 -0400

    Updated SQL Data Generation Files; Updated processingclients

--- SQL Files/clientsData.sql
+++ SQL Files/clientsData.sql
@@ -1,2 +1,3 @@
-INSERT INTO `clients`('CLIENTID', `FIRSTNAME`, `LASTNAME`, `ADDRESS`, `CITY`, `STATE`, `ZIP`, `PHONE`, `CELLPHONE`, `REFERRAL`, `EMAIL`)
-VALUES (1,Aaron,Butler,635 Somewhere,l-ville,GA,30062,180059963,8996852568,SamAllen,aaron@email.com);
+INSERT INTO clients(CLIENTID, FIRSTNAME, LASTNAME, ADDRESS, CITY, STATE, ZIP, PHONE, CELLPHONE, REFERRAL, EMAIL) VALUES
+(1, 'Aaron', 'Butler', '635 Somewhere', 'l-ville', 'GA', '30062', '180059963', '8996852568', NULL, 'aaron@email.com'),
+(2, 'Arthur', 'Reed', '32 Some Street', 'Suwannee', 'GA', '30962', '1234509876', '2135468790', '1', 'art@email.com');
--- photograph/processing/processingclients.php
+++ photograph/processing/processingclients.php
@@ -10,17 +10,18 @@ $ZIP = $_POST['ZIP'];
 $PHONE = $_POST['PHONE'];
 $CELLPHONE = $_POST['CELLPHONE'];
 $EMAIL = $_POST['EMAIL'];
+$REFERRAL = $_POST['REFERRAL'];
 
 //if values are not empty, proceed to store them in the database
 if(!empty($FNAME) && !empty($LNAME) && !empty($ADDRESS) && !empty($CITY) && !empty($ZIP) && !empty($PHONE) && !empty($EMAIL))
 {
-		mysqli_query($dbc, "INSERT INTO client('FIRSTNAME', 'LASTNAME', 'ADDRESS', 'CITY', 'STATE', 'ZIP', 'PHONE', 'CELLPHONE', 'EMAIL')
-		VALUES ('$FNAME', '$LNAME', '$ADDRESS', '$CITY', '$STATE', '$ZIP', '$PHONE', '$CELLPHONE','$REFERRAL')");
-		echo " row  inserted, everything worked fine!";
-		echo "<a href = '../employeepage.php'>Click here to be redirected to the employee Page. </a>";
+		mysqli_query($dbc, "INSERT INTO clients('FIRSTNAME', 'LASTNAME', 'ADDRESS', 'CITY', 'STATE', 'ZIP', 'PHONE', 'CELLPHONE', 'EMAIL', 'REFERRAL')
+		VALUES ('$FNAME', '$LNAME', '$ADDRESS', '$CITY', '$STATE', '$ZIP', '$PHONE', '$CELLPHONE', '$EMAIL','$REFERRAL')");
+		echo "<meta http-equiv='refresh' content='0;url=../employeepage.php'/>";
 }
 	else{
-			echo "ERROR: you left some values in blank!";
+			echo "<h1 align='center'>ERROR: You left a required field blank!</h1>";
+			echo "<meta http-equiv='refresh' content='2;url=../forms/clientuserform.php'/>";
 		}
 	}
 	else
